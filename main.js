273eb5a370b1ae83b02ae85bde6a37991a36a7c2
Avery Nguyen <averynguyen@Averys-MacBook-Air.local>
2020-11-20 11:15:06 -0700

    added code

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  //could be data() es6 shorthand
+  data:function() {
+    return {
+      product: 'Socks',
+      description: 'A warm fuzzy pair of socks'
+    }
+  }
+})
